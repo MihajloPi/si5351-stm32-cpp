01ea540e85a4bb82387dca51713e84d438f2311b
MihajloPi - YT1GS <mihajlo.raspberrypi@gmail.com>
2026-03-29 11:25:54 +0200

    Port to STM32

--- src/si5351.h
+++ src/si5351.h
@@ -1,39 +1,38 @@
 /*
- * si5351.h - Si5351 library for Arduino
+ * si5351.h - Si5351 library for STM32F411 (HAL)
  *
- * Copyright (C) 2015 - 2019 Jason Milldrum <milldrum@gmail.com>
- *                           Dana H. Myers <k6jq@comcast.net>
+ * Ported from the Arduino library by Jason Milldrum <milldrum@gmail.com>
+ *                                    Dana H. Myers <k6jq@comcast.net>
  *
+ * Original copyright (C) 2015 - 2019 Jason Milldrum / Dana H. Myers
  * Many defines derived from clk-si5351.h in the Linux kernel.
- * Sebastian Hesselbarth <sebastian.hesselbarth@gmail.com>
- * Rabeeh Khoury <rabeeh@solid-run.com>
- *
- * do_div() macro derived from /include/asm-generic/div64.h in
- * the Linux kernel.
- * Copyright (C) 2003 Bernardo Innocenti <bernie@develer.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Porting notes:
+ *  - All Arduino / Wire dependencies removed.
+ *  - I2C access is done via STM32 HAL (HAL_I2C_Master_Transmit /
+ *    HAL_I2C_Master_Receive / HAL_I2C_IsDeviceReady).
+ *  - Pass your hi2c handle pointer to the constructor.
+ *  - HAL requires a left-shifted 8-bit I2C address; the constructor does
+ *    the shift automatically (i.e. pass the plain 7-bit address 0x60).
+ *  - Dynamic heap allocations (new/delete) replaced with stack arrays.
+ *  - HAL_Delay() used wherever a delay is needed.
  */
 
 #ifndef SI5351_H_
 #define SI5351_H_
 
-#include "Arduino.h"
-#include "Wire.h"
 #include <stdint.h>
+#include <stdbool.h>
+#include "stm32f4xx_hal.h"
 
-/* Define definitions */
+/* ------------------------------------------------------------------ */
+/* Register / constant definitions (unchanged from original)           */
+/* ------------------------------------------------------------------ */
 
 #define SI5351_BUS_BASE_ADDR            0x60
 #define SI5351_XTAL_FREQ                25000000
@@ -144,7 +143,7 @@
 #define SI5351_OUTPUT_CLK_DIV_32        5
 #define SI5351_OUTPUT_CLK_DIV_64        6
 #define SI5351_OUTPUT_CLK_DIV_128       7
-#define SI5351_OUTPUT_CLK_DIVBY4       (3<<2)
+#define SI5351_OUTPUT_CLK_DIVBY4        (3<<2)
 
 #define SI5351_SSC_PARAM0               149
 #define SI5351_SSC_PARAM1               150
@@ -187,149 +186,157 @@
 #define SI5351_XTAL_ENABLE              (1<<6)
 #define SI5351_MULTISYNTH_ENABLE        (1<<4)
 
+/* HAL I2C timeout in milliseconds */
+#define SI5351_I2C_TIMEOUT              100
 
-/* Macro definitions */
+/* ------------------------------------------------------------------ */
+/* Macros                                                               */
+/* ------------------------------------------------------------------ */
 
-//#define RFRAC_DENOM ((1L << 20) - 1)
 #define RFRAC_DENOM 1000000ULL
 
-/*
- * Based on former asm-ppc/div64.h and asm-m68knommu/div64.h
- *
- * The semantics of do_div() are:
- *
- * uint32_t do_div(uint64_t *n, uint32_t base)
- * {
- *      uint32_t remainder = *n % base;
- *      *n = *n / base;
- *      return remainder;
- * }
- *
- * NOTE: macro parameter n is evaluated multiple times,
- *       beware of side effects!
- */
+#define do_div(n, base) ({                      \
+    uint64_t __base = (base);                   \
+    uint64_t __rem;                             \
+    __rem = ((uint64_t)(n)) % __base;           \
+    (n) = ((uint64_t)(n)) / __base;            \
+    __rem;                                      \
+})
 
-# define do_div(n,base) ({                                      \
-        uint64_t __base = (base);                               \
-        uint64_t __rem;                                         \
-        __rem = ((uint64_t)(n)) % __base;                       \
-        (n) = ((uint64_t)(n)) / __base;                         \
-        __rem;                                                  \
- })
+/* ------------------------------------------------------------------ */
+/* Enumerations                                                         */
+/* ------------------------------------------------------------------ */
 
-/* Enum definitions */
-
-/*
- * enum si5351_variant - SiLabs Si5351 chip variant
- * @SI5351_VARIANT_A: Si5351A (8 output clocks, XTAL input)
- * @SI5351_VARIANT_A3: Si5351A MSOP10 (3 output clocks, XTAL input)
- * @SI5351_VARIANT_B: Si5351B (8 output clocks, XTAL/VXCO input)
- * @SI5351_VARIANT_C: Si5351C (8 output clocks, XTAL/CLKIN input)
- */
-/*
-enum si5351_variant {
-	SI5351_VARIANT_A = 1,
-	SI5351_VARIANT_A3 = 2,
-	SI5351_VARIANT_B = 3,
-	SI5351_VARIANT_C = 4,
+enum si5351_clock {
+    SI5351_CLK0, SI5351_CLK1, SI5351_CLK2, SI5351_CLK3,
+    SI5351_CLK4, SI5351_CLK5, SI5351_CLK6, SI5351_CLK7
 };
-*/
 
-enum si5351_clock {SI5351_CLK0, SI5351_CLK1, SI5351_CLK2, SI5351_CLK3,
-	SI5351_CLK4, SI5351_CLK5, SI5351_CLK6, SI5351_CLK7};
+enum si5351_pll { SI5351_PLLA, SI5351_PLLB };
 
-enum si5351_pll {SI5351_PLLA, SI5351_PLLB};
+enum si5351_drive {
+    SI5351_DRIVE_2MA, SI5351_DRIVE_4MA,
+    SI5351_DRIVE_6MA, SI5351_DRIVE_8MA
+};
 
-enum si5351_drive {SI5351_DRIVE_2MA, SI5351_DRIVE_4MA, SI5351_DRIVE_6MA, SI5351_DRIVE_8MA};
+enum si5351_clock_source {
+    SI5351_CLK_SRC_XTAL, SI5351_CLK_SRC_CLKIN,
+    SI5351_CLK_SRC_MS0,  SI5351_CLK_SRC_MS
+};
 
-enum si5351_clock_source {SI5351_CLK_SRC_XTAL, SI5351_CLK_SRC_CLKIN, SI5351_CLK_SRC_MS0, SI5351_CLK_SRC_MS};
+enum si5351_clock_disable {
+    SI5351_CLK_DISABLE_LOW, SI5351_CLK_DISABLE_HIGH,
+    SI5351_CLK_DISABLE_HI_Z, SI5351_CLK_DISABLE_NEVER
+};
 
-enum si5351_clock_disable {SI5351_CLK_DISABLE_LOW, SI5351_CLK_DISABLE_HIGH, SI5351_CLK_DISABLE_HI_Z, SI5351_CLK_DISABLE_NEVER};
+enum si5351_clock_fanout {
+    SI5351_FANOUT_CLKIN, SI5351_FANOUT_XO, SI5351_FANOUT_MS
+};
 
-enum si5351_clock_fanout {SI5351_FANOUT_CLKIN, SI5351_FANOUT_XO, SI5351_FANOUT_MS};
+enum si5351_pll_input { SI5351_PLL_INPUT_XO, SI5351_PLL_INPUT_CLKIN };
 
-enum si5351_pll_input {SI5351_PLL_INPUT_XO, SI5351_PLL_INPUT_CLKIN};
-
-/* Struct definitions */
+/* ------------------------------------------------------------------ */
+/* Structures                                                           */
+/* ------------------------------------------------------------------ */
 
 struct Si5351RegSet
 {
-	uint32_t p1;
-	uint32_t p2;
-	uint32_t p3;
+    uint32_t p1;
+    uint32_t p2;
+    uint32_t p3;
 };
 
 struct Si5351Status
 {
-	uint8_t SYS_INIT;
-	uint8_t LOL_B;
-	uint8_t LOL_A;
-	uint8_t LOS;
-	uint8_t REVID;
+    uint8_t SYS_INIT;
+    uint8_t LOL_B;
+    uint8_t LOL_A;
+    uint8_t LOS;
+    uint8_t REVID;
 };
 
 struct Si5351IntStatus
 {
-	uint8_t SYS_INIT_STKY;
-	uint8_t LOL_B_STKY;
-	uint8_t LOL_A_STKY;
-	uint8_t LOS_STKY;
+    uint8_t SYS_INIT_STKY;
+    uint8_t LOL_B_STKY;
+    uint8_t LOL_A_STKY;
+    uint8_t LOS_STKY;
 };
+
+/* ------------------------------------------------------------------ */
+/* Class                                                                */
+/* ------------------------------------------------------------------ */
 
 class Si5351
 {
 public:
-  Si5351(uint8_t i2c_addr = SI5351_BUS_BASE_ADDR);
-	bool init(uint8_t, uint32_t, int32_t);
-	void reset(void);
-	uint8_t set_freq(uint64_t, enum si5351_clock);
-	uint8_t set_freq_manual(uint64_t, uint64_t, enum si5351_clock);
-	void set_pll(uint64_t, enum si5351_pll);
-	void set_ms(enum si5351_clock, struct Si5351RegSet, uint8_t, uint8_t, uint8_t);
-	void output_enable(enum si5351_clock, uint8_t);
-	void drive_strength(enum si5351_clock, enum si5351_drive);
-	void update_status(void);
-	void set_correction(int32_t, enum si5351_pll_input);
-	void set_phase(enum si5351_clock, uint8_t);
-	int32_t get_correction(enum si5351_pll_input);
-	void pll_reset(enum si5351_pll);
-	void set_ms_source(enum si5351_clock, enum si5351_pll);
-	void set_int(enum si5351_clock, uint8_t);
-	void set_clock_pwr(enum si5351_clock, uint8_t);
-	void set_clock_invert(enum si5351_clock, uint8_t);
-	void set_clock_source(enum si5351_clock, enum si5351_clock_source);
-	void set_clock_disable(enum si5351_clock, enum si5351_clock_disable);
-	void set_clock_fanout(enum si5351_clock_fanout, uint8_t);
-	void set_pll_input(enum si5351_pll, enum si5351_pll_input);
-	void set_vcxo(uint64_t, uint8_t);
-  void set_ref_freq(uint32_t, enum si5351_pll_input);
-	uint8_t si5351_write_bulk(uint8_t, uint8_t, uint8_t *);
-	uint8_t si5351_write(uint8_t, uint8_t);
-	uint8_t si5351_read(uint8_t);
-	struct Si5351Status dev_status = {.SYS_INIT = 0, .LOL_B = 0, .LOL_A = 0,
-    .LOS = 0, .REVID = 0};
-	struct Si5351IntStatus dev_int_status = {.SYS_INIT_STKY = 0, .LOL_B_STKY = 0,
-    .LOL_A_STKY = 0, .LOS_STKY = 0};
-	enum si5351_pll pll_assignment[8];
-	uint64_t clk_freq[8];
-	uint64_t plla_freq;
-	uint64_t pllb_freq;
-  enum si5351_pll_input plla_ref_osc;
-  enum si5351_pll_input pllb_ref_osc;
-	uint32_t xtal_freq[2];
+    /*
+     * Constructor.
+     *
+     * hi2c     - Pointer to the HAL I2C handle (e.g. &hi2c1).
+     * i2c_addr - 7-bit I2C device address (default 0x60).
+     *            The constructor shifts it left to form the 8-bit HAL address.
+     */
+    Si5351(I2C_HandleTypeDef *hi2c, uint8_t i2c_addr = SI5351_BUS_BASE_ADDR);
+
+    bool    init(uint8_t xtal_load_c, uint32_t ref_osc_freq, int32_t corr);
+    void    reset(void);
+    uint8_t set_freq(uint64_t freq, enum si5351_clock clk);
+    uint8_t set_freq_manual(uint64_t freq, uint64_t pll_freq, enum si5351_clock clk);
+    void    set_pll(uint64_t pll_freq, enum si5351_pll target_pll);
+    void    set_ms(enum si5351_clock clk, struct Si5351RegSet ms_reg,
+                   uint8_t int_mode, uint8_t r_div, uint8_t div_by_4);
+    void    output_enable(enum si5351_clock clk, uint8_t enable);
+    void    drive_strength(enum si5351_clock clk, enum si5351_drive drive);
+    void    update_status(void);
+    void    set_correction(int32_t corr, enum si5351_pll_input ref_osc);
+    void    set_phase(enum si5351_clock clk, uint8_t phase);
+    int32_t get_correction(enum si5351_pll_input ref_osc);
+    void    pll_reset(enum si5351_pll target_pll);
+    void    set_ms_source(enum si5351_clock clk, enum si5351_pll pll);
+    void    set_int(enum si5351_clock clk, uint8_t enable);
+    void    set_clock_pwr(enum si5351_clock clk, uint8_t pwr);
+    void    set_clock_invert(enum si5351_clock clk, uint8_t inv);
+    void    set_clock_source(enum si5351_clock clk, enum si5351_clock_source src);
+    void    set_clock_disable(enum si5351_clock clk, enum si5351_clock_disable dis_state);
+    void    set_clock_fanout(enum si5351_clock_fanout fanout, uint8_t enable);
+    void    set_pll_input(enum si5351_pll pll, enum si5351_pll_input input);
+    void    set_vcxo(uint64_t pll_freq, uint8_t ppm);
+    void    set_ref_freq(uint32_t ref_freq, enum si5351_pll_input ref_osc);
+
+    /* Low-level I2C access (public for advanced users) */
+    HAL_StatusTypeDef si5351_write_bulk(uint8_t addr, uint8_t bytes, uint8_t *data);
+    HAL_StatusTypeDef si5351_write(uint8_t addr, uint8_t data);
+    uint8_t           si5351_read(uint8_t addr);
+
+    /* Public status structs */
+    struct Si5351Status    dev_status    = {0, 0, 0, 0, 0};
+    struct Si5351IntStatus dev_int_status = {0, 0, 0, 0};
+
+    enum si5351_pll pll_assignment[8];
+    uint64_t clk_freq[8];
+    uint64_t plla_freq;
+    uint64_t pllb_freq;
+    enum si5351_pll_input plla_ref_osc;
+    enum si5351_pll_input pllb_ref_osc;
+    uint32_t xtal_freq[2];
+
 private:
-	uint64_t pll_calc(enum si5351_pll, uint64_t, struct Si5351RegSet *, int32_t, uint8_t);
-	uint64_t multisynth_calc(uint64_t, uint64_t, struct Si5351RegSet *);
-	uint64_t multisynth67_calc(uint64_t, uint64_t, struct Si5351RegSet *);
-	void update_sys_status(struct Si5351Status *);
-	void update_int_status(struct Si5351IntStatus *);
-	void ms_div(enum si5351_clock, uint8_t, uint8_t);
-	uint8_t select_r_div(uint64_t *);
-	uint8_t select_r_div_ms67(uint64_t *);
-	int32_t ref_correction[2];
-  uint8_t clkin_div;
-  uint8_t i2c_bus_addr;
-  bool clk_first_set[8];
+    uint64_t pll_calc(enum si5351_pll pll, uint64_t freq,
+                      struct Si5351RegSet *reg, int32_t correction, uint8_t vcxo);
+    uint64_t multisynth_calc(uint64_t freq, uint64_t pll_freq, struct Si5351RegSet *reg);
+    uint64_t multisynth67_calc(uint64_t freq, uint64_t pll_freq, struct Si5351RegSet *reg);
+    void     update_sys_status(struct Si5351Status *status);
+    void     update_int_status(struct Si5351IntStatus *int_status);
+    void     ms_div(enum si5351_clock clk, uint8_t r_div, uint8_t div_by_4);
+    uint8_t  select_r_div(uint64_t *freq);
+    uint8_t  select_r_div_ms67(uint64_t *freq);
+
+    int32_t           ref_correction[2];
+    uint8_t           clkin_div;
+    uint8_t           i2c_bus_addr;   /* 8-bit HAL address (7-bit << 1) */
+    bool              clk_first_set[8];
+    I2C_HandleTypeDef *hi2c;          /* HAL I2C handle */
 };
 
 #endif /* SI5351_H_ */
